e0abd611b40f551400800078df4abda39a9afac1
Fernando <fernandogm@live.com.ar>
2020-09-01 22:49:06 -0300

    dos ejercicios var local ref out

--- 2020-09-01/clase3/Program.cs
+++ 2020-09-01/clase3/Program.cs
@@ -10,34 +10,87 @@ namespace clase3
             int winner = random.Next(10)+1;
             int cont = 0;
             int apuesta;
+
+            
             do
             {
 
-                Console.WriteLine("Tenes tres intentos, Intento N°:"+(cont+1));
-                Console.WriteLine("adivine el numero entre 1 y 10");
-                
-                Console.WriteLine(winner);
-                apuesta = int.Parse(Console.ReadLine());
+                apuesta = Preguntar("ingrese un numero de 1 a 10:", out apuesta);
                 cont++;
-
                
-                    if (winner == apuesta)
+
+                if (winner == apuesta)
                     {
-                        Console.WriteLine("Ganaste!");
+                        Console.WriteLine(Ganaste());
                     }
                     else
                     {
                         Console.WriteLine("Estuvo cerca, vuelve a intentar!");
                     if (cont == 3) {
-                        Console.WriteLine("Perdiste!");
+                        Console.WriteLine(Perdiste(winner));
                     }
                     }
                 
               
                 } while (winner != apuesta && cont <= 2);
 
-          
+            
+
+            int numero;
+            numero = Fecha("ingrese su año de nacimiento y compruebe si es bisiesto: ");
+            bool checker;
+            CalcularAñoBisiesto(numero, out checker);
+            if (checker)
+            {
+                Console.WriteLine("el año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("el año no es bisiesto");
+            }
 
         }
+        static int Fecha(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            string val = Console.ReadLine();
+            return int.Parse(val);
+
+        }
+
+        static Boolean CalcularAñoBisiesto(int anio, out bool checker)
+        {
+            if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0)))
+                checker = true;
+            else
+                checker = false;
+
+            return checker;
+        }
+        
+        static String Ganaste()
+        {
+            return "ganaste!";
+        }
+
+        static string Perdiste( int valor)
+        {
+            return "Perdiste!, el numero correcto era: " + valor;
+        }
+
+        static int Preguntar(string mensaje, out int apuesta)
+        {
+            Console.WriteLine(mensaje);
+            string valor= Console.ReadLine();
+
+            apuesta=  int.Parse(valor);
+            return apuesta;
+
+
+        }
+
     }
+
+
+ 
 }
